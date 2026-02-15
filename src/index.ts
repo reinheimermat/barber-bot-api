7900c2136ddd5bcad043e57c95a0e8b46de69ef8
Matheus Reinheimer <mreinheimer0@gmail.com>
2026-02-15 20:32:50 -0300

    config host

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,9 @@ app.use(availableHoursController)
 
 app.use(setBookingFlowController)
 
-app.listen(3333)
+app.listen({
+  port: parseInt(process.env.PORT || '3333'),
+  hostname: '0.0.0.0'
+})
 
 console.log(`Elysia is running at ${app.server?.hostname}:${app.server?.port}`)
